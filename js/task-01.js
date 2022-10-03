ef80e8e9c27a4bcbf780b17c362c4b65772302b2
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-03 18:27:11 +0300

    + Task2

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,8 @@
-// Number of categories: 3
+const categoriesRef = document.querySelectorAll('#categories .item');
+console.log('Number of categories:', categoriesRef.length);
 
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
-
-const categories = document.querySelectorAll('#categories .item');
-console.log('Number of categories:', categories.length);
-
-categories.forEach(category => {
-    console.log('');
-    console.log('Category:', category.querySelector('h2').textContent);
-    console.log('Elements:', category.querySelectorAll('li').length);
+categoriesRef.forEach(category => {
+  console.log('');
+  console.log('Category:', category.querySelector('h2').textContent);
+  console.log('Elements:', category.querySelectorAll('li').length);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = document.querySelector('#ingredients');
+const arrToAddEl = [];
+ingredients.forEach(ingredient => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add('item');
+  arrToAddEl.push(ingredientEl);
+});
+listRef.append(...arrToAddEl);
