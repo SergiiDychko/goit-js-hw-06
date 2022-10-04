00ac4a71948e378d43cdfb6fb47dd216af1c16cb
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-04 14:50:38 +0300

    + Task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return console.log('Всі поля повинні бути заповнені');
+  }
+
+  console.log(`E-mail: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+form.addEventListener('submit', handleSubmit);
