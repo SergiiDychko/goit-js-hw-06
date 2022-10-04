0abafe95e707975c4eaddda963779e3026d30807
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-04 12:05:35 +0300

    + Task6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputRef = document.querySelector('#validation-input');
+const validation = () => {
+    if (inputRef.value.length === Number(inputRef.dataset.length)) {
+        inputRef.classList.remove('invalid');
+        inputRef.classList.add('valid')
+    }
+    else {
+        inputRef.classList.remove('valid');
+        inputRef.classList.add('invalid');
+    } 
+}
+
+inputRef.addEventListener('blur', validation);
+
