2e0620ba8a11769401d57f6a2f448c9abbdd1eac
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-04 16:07:21 +0300

    + Task9

--- js/task-08.js
+++ js/task-08.js
@@ -2,9 +2,7 @@ const form = document.querySelector('.login-form');
 
 function handleSubmit(event) {
   event.preventDefault();
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
+  const { email, password } = event.target.elements;
 
   if (email.value === '' || password.value === '') {
     return console.log('Всі поля повинні бути заповнені');
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnRef = document.querySelector('.change-color');
+const colorValueRef = document.querySelector('.color');
+
+const getChangeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+const getChangeText = () => {
+  colorValueRef.textContent = getRandomHexColor();
+};
+
+changeColorBtnRef.addEventListener('click', getChangeColor);
+changeColorBtnRef.addEventListener('click', getChangeText);
