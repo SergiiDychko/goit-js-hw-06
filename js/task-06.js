483b6f174c584e0d9893f9fc7607adc000514202
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-04 17:36:47 +0300

    + Task10

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,12 @@
 const inputRef = document.querySelector('#validation-input');
-const validation = () => {
-    if (inputRef.value.length === Number(inputRef.dataset.length)) {
-        inputRef.classList.remove('invalid');
-        inputRef.classList.add('valid')
-    }
-    else {
-        inputRef.classList.remove('valid');
-        inputRef.classList.add('invalid');
+
+const validation = (event) => {
+    const {value, dataset, classList} = event.target;
+    if (value.length === Number(dataset.length)) {
+      classList.remove('invalid');
+      classList.add('valid');
+    } else {
+      classList.add('invalid');
     } 
 }
 
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtnRef = document.querySelector('button[data-create]');
+const destroyBtnRef = document.querySelector('button[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+const createBoxes = () => {
+  const amount = document.querySelector('#controls input').value;
+  const boxesArr = [];
+  let divSize = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    boxesArr.push(
+      `<div style="width:${divSize}px; height:${divSize}px; background-color:${getRandomHexColor()}"></div>`
+    );
+    divSize += 10;
+  }
+  boxesRef.insertAdjacentHTML('beforeend', boxesArr.join(''));
+}
+
+const destroyBoxes = () => {
+boxesRef.innerHTML = '';
+}
+
+createBtnRef.addEventListener('click', createBoxes)
+destroyBtnRef.addEventListener('click', destroyBoxes);
